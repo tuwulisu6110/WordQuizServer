a6adc85e67937f12d24d3b7fc718933cc54a3fd9
Boris Chang <tuwulisu6110@gmail.com>
2021-01-19 22:28:20 +0800

    Added self-signed cert

--- WebServer/WebServer.py
+++ WebServer/WebServer.py
@@ -46,4 +46,4 @@ def listWordPage():
     return render_template('listWordPage.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',ssl_context='adhoc')
+    app.run(host='0.0.0.0',ssl_context=('cert.pem', 'key.pem'))
